298f24f8178a0b279dae3162441e00a69c9b7146
Asskicker25 <sp68860@gmail.com>
2023-10-26 06:24:37 -0400

    Cube with left pivot loaded

--- Berserk_Patterns_Midterm/Berserk_Patterns_Midterm/src/Maze/Maze.cpp
+++ Berserk_Patterns_Midterm/Berserk_Patterns_Midterm/src/Maze/Maze.cpp
@@ -24,6 +24,24 @@ void Maze::RemoveFromPhysics(PhysicsEngine& physicsEngine)
 void Maze::AddToRendererAndPhysics(Renderer& renderer, Shader* shader, PhysicsEngine& physicsEngine)
 {
 	mazeModel->LoadModel("Assets/Models/Berzerk_Level_Ouput.ply");
-
 	renderer.AddModel(mazeModel, shader);
+
+#pragma region Colliders
+	//Bottom Border
+	Model* cube1 = new Model("Assets/Models/CubeWithLeftPivot.fbx");
+	cube1->transform.SetPosition(glm::vec3(0.0f - 0.5f, 0.0f - 0.5f, 1.0f));
+	cube1->transform.SetScale(glm::vec3(100.0f, 1.0f, 1.0f));
+
+
+	//Top Border
+	/*Model* cube2 = new Model();
+	cube2 = &(*cube1);
+	cube2->transform.SetPosition(glm::vec3(0.0f, 60.0f, 1.0f));*/
+
+
+	renderer.AddModel(cube1, shader);
+#pragma endregion
+
+	
+
 }
